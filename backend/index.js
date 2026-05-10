fecdd644adf40df4978c20ca9247ca5dcc31ed4e
Iswardahal142 <junmain92@gmail.com>
2026-05-10 09:50:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -114,6 +114,7 @@ function initClient() {
     }),
     puppeteer: {
       headless: true,
+      executablePath: process.env.CHROME_PATH || "/usr/bin/google-chrome-stable",
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
